b99bb5df3ff12fafa2ceacda471fae8f9b6ff0d1
Ankita Kumari <Ankitak28@gmail.com>
2021-02-17 23:23:37 +0530

    Contacts Pojo class added

---
+++ ContactsApp/app/src/main/java/com/example/contactsapp/Contact.java
@@ -0,0 +1,47 @@
+package com.example.contactsapp;
+
+public class Contact {
+    String name;
+    String city;
+    int age;
+    long phone;
+
+    public Contact(String name, String city, int age, long phone) {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+}
